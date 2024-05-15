a997700ced7d56c3392d99596f473c29c08f7d29
Harris Goodhand <hgoodlyhand@hotmail.com>
2024-05-15 11:26:07 -0400

    added register/login routes, added user nickname

--- db/auth/index.js
+++ db/auth/index.js
@@ -2,21 +2,54 @@ import express, { Router } from 'express'
 import { PrismaClient } from '@prisma/client'
 import jsonwebtoken from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import 'dotenv/config'
 
 const jwt = jsonwebtoken
 const prisma = new PrismaClient()
 const authRouter = express.Router()
 
-Router.post("/register", async (req, res, next) => {
-  const {username, password } = req.body
+authRouter.post("/register", async (req, res, next) => {
+  const {username, password, nickname } = req.body
   const hashedPassword = await bcrypt.hash(password, 10)
   try{
     const userToRegister = await prisma.user.create({
       data: {
         username: username,
         password: hashedPassword,
+        nickname: nickname
       }
     })
+    const token = jwt.sign({ id: userToRegister.id }, process.env.JWT_SECRET)
+    res.send({ token })
+  }catch (error){
+    console.log(error)
+    next(error)
+  }
+})
+
+authRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username: username
+    }
+  })
+  const pwCompare = await bcrypt.compare(password, foundUser.password)
+  if(pwCompare !== true){
+    return res.send("Invalid login credentials")
+  }
+
+  try{
+    const userToLogin = await prisma.user.findUnique({
+      where: {
+        username: username,
+        password: foundUser.password
+      }
+    })
+
+    const token = jwt.sign({ id: userToLogin.id }, process.env.JWT_SECRET)
+    console.log("Logged in")
+    res.send({ token })
   }catch (error){
     console.log(error)
     next(error)
