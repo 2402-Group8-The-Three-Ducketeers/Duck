342feb5a9a1f3990adcede3168e0a7f2c8f960f6
Harris Goodhand <hgoodlyhand@hotmail.com>
2024-05-21 11:17:04 -0400

    removed bad import

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,10 @@
-import { response } from 'express'
 import { useEffect, useState } from 'react'
 
 const Profile = () => {
-  const [token, setToken] = useState('')
-  //temporary token procedure until the login page sets tokens
-  const getTokenFromSessionStorage = async () => {
-    let headers = {}
-    if (sessionStorage.token) {
-        headers = { 'Authorization': `Bearer ${sessionStorage.token}` }
-    }
-    try{
-      const response = await fetch("/getloggedinuser/", { headers: headers })
-      const responseJSON = await response.json()
-      if(responseJSON.token){
-        setToken(responseJSON.token)
-      }
-    }catch (error){
-      throw error
-    }
-  }
-  getTokenFromSessionStorage()
+  
+  const token = JSON.parse(localStorage.getItem('currentUser'))
+  
+
   console.log("token: ", token)
   return (
     <>
