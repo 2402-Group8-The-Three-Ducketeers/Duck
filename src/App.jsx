b1040f857130f6f77b65249712117920b8a86a14
Harris Goodhand <hgoodlyhand@hotmail.com>
2024-05-22 12:35:41 -0400

    fixed way game displays on server

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ import Home from "./pages/Home"
 import LogIn from "./pages/LogIn"
 import Register from "./pages/Register"
 import GamePage from "./pages/GamePage"
-import Test from './pages/Test'
-
-import duckSpritePath from "./components/images/ducksprite.png"
-import cloudSpritePath from "./components/images/cloudsprite.png"
-import portalSpritePath from "./components/images/portal.png"
-import pterodactylSpritePath from "./components/images/pterodactyl.png"
-import grassSpritePath from "./components/images/grass.png"
-import sandSpritePath from "./components/images/sand.png"
-import eagleSpritePath from "./components/images/eagle.png"
-import groundSpritePath from "./components/images/ground.png"
-import lavaSpritePath from "./components/images/lava.png"
 
 function App() {
   
@@ -27,7 +16,6 @@ function App() {
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/game" element={<GamePage/>}/>
-        <Route path="/test" element={<Test/>}/>
       </Routes>
     </>
   )
