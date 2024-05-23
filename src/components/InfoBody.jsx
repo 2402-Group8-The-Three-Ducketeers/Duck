2b20fb618c4d07994e175a0b162ab0c6a905c4f2
Carlos Perez <cmperez0730@gmail.com>
2024-05-23 11:58:18 -0700

    Body of the Info page in progress

--- src/components/InfoBody.jsx
+++ src/components/InfoBody.jsx
@@ -1,6 +1,22 @@
 const InfoBody = () => {
   return (
-    <></>
+    <>
+    <section>
+      <h1>Story</h1>
+      <h2>Find out the lore of this game. Find out why you are on this adventure and learn about the extensive lore of the game's characters. With this you will understand the entire development of the game's adventure.</h2>
+      <button>Lore</button>
+    </section>
+    <section>
+      <h1>Tutorial</h1>
+      <h2>Learn the controls of the game. Now your game's goals and how to reach them.</h2>
+      <button>How to Play</button>
+    </section>
+    <section>
+      <h1>Credits</h1>
+      <h2>Meet the masterminds behind this work of art.</h2>
+      <button>Creators</button>
+    </section>
+    </>
   )
 }
 
