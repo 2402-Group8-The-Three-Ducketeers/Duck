8981a3c9edcf33c70cb3bc5fb6490e359eadc698
Harris Goodhand <hgoodlyhand@hotmail.com>
2024-05-24 14:45:35 -0400

    starter code for restoring token after page refresh

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,16 @@ import Leaderboard from "./pages/Leaderboard"
 import GamePage from "./pages/GamePage"
 import Admin from "./pages/Admin"
 import Info from './pages/Info'
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  
+  const token = useSelector(state => state.authorization.token);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if(!token){
+      dispatch(setToken(localStorage.getItem('token')))
+    }
+  }, [])
 
   return (
     <>
